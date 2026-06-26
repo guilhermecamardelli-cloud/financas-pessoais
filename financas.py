d57893ff476aa96c9830b79e01e6db91ee3719f5
Guigo <guilherme.camardelli@gmail.com>
2026-06-26 19:21:12 -0300

    Update financas.py

--- financas.py
+++ financas.py
@@ -16,3 +16,27 @@ def carregar ():
             )
             return []
     return []
+
+def salvar (lancamentos):
+    try:
+        with open(ARQUIVO_JSON, "w", encoding="utf-8") as f:
+            json.dump(lancamentos, f, indent=4, ensure_ascii=False)
+    except IOError:
+        print("Erro ao salvar os dados no arquivo.")
+
+def registrar_lancamento(lancamentos):
+    print("\n--- REGISTRAR NOVA MOVIMENTAÇÃO ---")
+    while True:
+        tipo = input("Digite o tipo (receita/despesa): ").strip().lower()
+        if tipo in ["receita", "despesa"]:
+            break
+        print("Entrada inválida! Digite 'receita' ou 'despesa'.")
+    while True:
+        try:
+            valor = float(input("Digite o valor (Ex: 150.50): "))
+            if valor <= 0:
+                print("O valor deve ser maior que zero.")
+                continue
+            break
+        except ValueError:
+            print("Entrada inválida! Digite um número válido.")
