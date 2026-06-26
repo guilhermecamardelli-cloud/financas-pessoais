6b12457f8448794e504bf1063f89eb8e2e9239f5
Guigo <guilherme.camardelli@gmail.com>
2026-06-26 19:09:39 -0300

    Update financas.py

--- financas.py
+++ financas.py
@@ -3,3 +3,16 @@ import os
 from datetime import datetime
 
 ARQUIVO = "lancamentos.json"
+ARQUIVO_TXT = "relatorio.txt"
+
+def carregar ():
+    if os.path.exists(ARQUIVO_JSON):
+        try:
+            with open(ARQUIVO_JSON, "r", encoding="utf-8") as f:
+                return json.load(f)
+        except json.JSONDecodeError:
+            print(
+                "Erro ao ler o arquivo de dados. Inicializando lista vazia."
+            )
+            return []
+    return []
